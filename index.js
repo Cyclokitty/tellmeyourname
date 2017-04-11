c874fd58397ddd8ca356f5c7524c1846106bd4f5
Laura Vingada <laura@vingada.com>
2017-04-10 20:47:15 -0400

    added views and public folders and files.

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
+const engines = require('consolidate');
 
 const port = process.env.PORT || 3000;
 
+app.engine('html', engines.nunjucks);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => {
-  res.send('This was made by Laura Veee');
+  res.render('index', {});
 });
 
 app.listen(port, () => {
