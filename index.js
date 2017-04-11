0b2d43a69e2de5667595d35cbc709e09f5140f6a
Laura Vingada <laura@vingada.com>
2017-04-10 20:12:53 -0400

    added some libraries

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Here it is!');
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('This was made by Laura Veee');
+});
+
+app.listen(port, () => {
+  console.log(`App open and listening on port ${port}.`);
+});
