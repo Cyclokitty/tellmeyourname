75a38656fe22dd07db575a2cf2e49e99c4af6e7f
Laura Vingada <laura@vingada.com>
2017-04-10 21:33:46 -0400

    added body-parser and results page. It works

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const engines = require('consolidate');
 
 const port = process.env.PORT || 3000;
 
 app.engine('html', engines.nunjucks);
 app.set('view engine', 'html');
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
   res.render('index', {});
 });
+
+app.post('/', (req, res) => {
+  const name = req.body.name;
+  res.render('results.html', {name: `${name}`});
+})
 
 app.listen(port, () => {
   console.log(`App open and listening on port ${port}.`);
