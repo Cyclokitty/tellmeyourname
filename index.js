d742f8675d9d0eb4b901775ffd8ab340a30b90b2
Laura Vingada <laura@vingada.com>
2017-04-12 18:24:58 -0400

    added mongodb and can save names

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const engines = require('consolidate');
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+const dotenv = require('dotenv').config();
 
 const port = process.env.PORT || 3000;
 
@@ -15,14 +18,24 @@ app.get('/', (req, res) => {
   res.render('index', {});
 });
 
-app.post('/', (req, res) => {
-  const name = req.body.name;
-  if (name === '') {
-    res.render('help.html', {});
-  } else {
-    res.render('results.html', {name: `${name}`});
-  }
-});
+MongoClient.connect(process.env.MONGO, (err, db) => {
+  assert.equal(null, err);
+
+  app.post('/', (req, res) => {
+    const name = req.body.name;
+    if (name === '') {
+      res.render('help.html', {});
+    } else {
+      db.collection('words').insertOne({
+        'name': name
+      },
+      function (err, r) {
+        assert.equal(null, err);
+      });
+      res.render('results.html', {name: `${name}`});
+    }
+  });
+})
 
 app.listen(port, () => {
   console.log(`App open and listening on port ${port}.`);
